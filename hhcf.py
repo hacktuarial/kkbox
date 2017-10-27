1f5ee1ae723231fb761c475d3802a6746743081d
Timothy Sweetser <tsweetser@gmail.com>
2017-10-27 07:12:45 -0700

    refactor into utils

--- hhcf.py
+++ hhcf.py
@@ -23,159 +23,18 @@
 
 import numpy as np
 import pandas as pd
-import pickle
 import click
 
+import xgboost as xgb
 from scipy import sparse
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import roc_auc_score
 
-from diamond.glms.logistic import LogisticRegression
-import xgboost as xgb
 import utils
-import dill
 import os
 
 import logging
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
                     level=logging.INFO)
-
-
-class ExtraInfo(object):
-    """ holds info about songs or members for making recs """
-    def __init__(self, df):
-        self.df = df
-        self.encoding = None
-        self.id_map = None
-
-    def create_encoding(self, cat_features, id_col):
-        """
-        one hot encode categorical features. modifies `self.df` in-place
-        """
-        idx = id_col + 'x'
-        for x in cat_features:
-            self.df[x] = self.df[x].astype(str).fillna('unknown')
-        self.id_map = id_to_index(self.df[id_col])
-        self.df[idx] = self.df[id_col].apply(lambda x: self.id_map[x])
-        cats = self.df[cat_features + [idx]].sort_values(idx)
-        self.X = DictVectorizer().fit_transform(cats.T.to_dict().values())
-
-    def save(self, f):
-        with open(f, 'wb') as ff:
-            dill.dump(self, ff)
-
-    def load(self, f):
-        with open(f, 'rb') as ff:
-            self.__dict__.update(dill.load(ff).__dict__)
-
-
-def id_to_index(ids):
-    " return a dict like {id: idx} "
-    unique_ids = set(ids)
-    return dict(zip(unique_ids, np.arange(0, len(unique_ids))))
-
-
-def encode_genres(song_map, df_songs):
-    " this is a many-hot encoding of song to genre "
-    path = 'data/processed/genres.dill'
-    if os.path.exists(path):
-        logging.info('reading genre encodings from disk')
-        with open(path, 'rb') as ff:
-            X_genres = dill.load(ff)
-    else:
-        logging.info('creating genre encodings')
-        df_genres = df_songs['genre_ids'].apply(lambda s: pd.Series(str(s).split('|')))
-        df_genres['song_idx'] = df_songs['song_id'].apply(lambda x: song_map[x])
-        df_genres2 = pd.melt(df_genres, 'song_idx', value_name='genre').\
-            drop('variable', axis=1).\
-            dropna().\
-            sort_values('song_idx')
-        genre_map = id_to_index(df_genres2['genre'])
-        df_genres2['genre_idx'] = df_genres2['genre'].apply(lambda g: genre_map[g])
-        X_genres = sparse.coo_matrix((np.ones(len(df_genres2)),
-                                     (df_genres2['song_idx'], df_genres2['genre_idx'])))
-        X_genres = X_genres.tocsr()
-        with open(path, 'wb') as ff:
-            dill.dump(X_genres, ff)
-    return X_genres
-
-
-def xgb_params():
-    xgb_params = {}
-    xgb_params['objective'] = 'binary:logistic'
-    xgb_params['eta'] = 0.75
-    xgb_params['max_depth'] = 5
-    xgb_params['silent'] = 1
-    xgb_params['eval_metric'] = 'auc'
-    return xgb_params
-
-
-def merge_it_all_together(df,
-                          songs,
-                          genres,
-                          members,
-                          matrix_list,
-                          diamond):
-    """
-    Merge member, song, and source data together
-    Return (X, y) tuple
-    This makes a _copy_ of df
-    """
-
-    # there are 80 interactions with unknown songs in the training set
-    # remove them!
-    logging.info('merging in song info')
-    df = pd.merge(df.drop(SOURCE_FEATURES, axis=1),
-                  songs.df[['song_id', 'song_idx', 'song_length']],
-                  'inner',
-                  'song_id')
-    logging.info('merging in member info')
-    member_features = ['msno', 'msnox', 'registration_init_time',
-                       'expiration_date', 'bd']
-    df = pd.merge(df, members.df[member_features], how='left', on='msno')
-    n_nulls = df.isnull().sum()
-    assert n_nulls.sum() == 0, n_nulls
-    logging.info('there are no nulls')
-
-    numeric_features = ['song_length', 'registration_init_time',
-                        'expiration_date', 'bd']
-
-    X_numeric = df[numeric_features].as_matrix()
-    member_idx = df['msnox']
-    song_idx = df['song_idx']
-    y = df['target']
-    del df  # free up the memory
-
-    logging.info('adding numeric feature matrix')
-    matrix_list.append(X_numeric)
-    logging.info('indexing song matrix')
-    matrix_list.append(songs.X[song_idx, :])
-    logging.info('indexing genre matrix')
-    matrix_list.append(genres[song_idx, :])
-    logging.info('indexing member matrix')
-    matrix_list.append(members.X[member_idx, :])
-    logging.info('concatenating matrices')
-    X = sparse.hstack(matrix_list)
-    return X, y
-
-
-def get_source_encoding(source_features, df):
-    path = 'data/processed/source_features.dill'
-    if os.path.exists(path):
-        logging.info('reading source feature encodings from disk')
-        with open(path, 'rb') as ff:
-            X_source = dill.load(ff)
-    else:
-        logging.info('creating source features encoding')
-        # TODO save to disk
-        LE, OHE = LabelEncoder(), OneHotEncoder()
-        X_source = OHE.fit_transform(df[source_features].\
-            fillna('unknown').\
-            apply(LE.fit_transform))
-        with open(path, 'wb') as ff:
-            dill.dump(X_source, ff)
-    return X_source
 
 
 MAX_ITS = 200
@@ -190,30 +49,29 @@ def main(diamond):
     df_train = pd.read_csv('data/raw/train.csv')
     if os.path.isfile('data/processed/songs.dill'):
         logging.info('reading song encoding from disk')
-        songs = ExtraInfo(df=None)
+        songs = utils.ExtraInfo(df=None)
         songs.load('data/processed/songs.dill')
     else:
         logging.info('creating song encoding')
-        songs = ExtraInfo(df=pd.read_csv('data/raw/songs.csv'))
+        songs = utils.ExtraInfo(df=pd.read_csv('data/raw/songs.csv'))
         songs.create_encoding(id_col='song_id',
                               cat_features=['artist_name', 'composer', 'lyricist', 'language'])
         songs.save('data/processed/songs.dill')
     if os.path.isfile('data/processed/members.dill'):
         logging.info('reading member encoding from disk')
-        members = ExtraInfo(df=None)
+        members = utils.ExtraInfo(df=None)
         members.load('data/processed/members.dill')
     else:
-        members = ExtraInfo(df=pd.read_csv('data/raw/members.csv'))
+        members = utils.ExtraInfo(df=pd.read_csv('data/raw/members.csv'))
         members.create_encoding(id_col='msno',
                                 cat_features=['city', 'gender', 'registered_via'])
         members.save('data/processed/members.dill')
 
     # ### IDs: benchmark for diamond
     if diamond:
-        numeric_features = ['diamond_prediction']
         matrix_list = []
-        df_train, df_val = train_test_split(df_train)
-        diamond_model = fit_diamond_model(df_train)
+        df_train, df_val = utils.train_test_split(df_train)
+        diamond_model = utils.fit_diamond_model(df_train)
         df_train['diamond_pred'] = diamond_model.predict(df_train)
         df_val['diamond_pred'] = diamond_model.predict(df_val)
     else:
@@ -228,67 +86,38 @@ def main(diamond):
                        X_song_ids[song_idx, :]]
 
     # ### Genres
-    X_genres = encode_genres(songs.id_map, songs.df)
+    X_genres = utils.encode_genres(songs.id_map, songs.df)
 
     # ### Source features
     # Thanks to [Ritchie Ng](http://www.ritchieng.com/machinelearning-one-hot-encoding/) for guidance on using One Hot Encoder
-    X_source = get_source_encoding(source_features=SOURCE_FEATURES,
+    X_source = utils.get_source_encoding(source_features=SOURCE_FEATURES,
                                    df=df_train)
 
     logging.info('merging validation data together')
-    del df_train
-    X_val, y_val = merge_it_all_together(df_val,
-                                         diamond=diamond,
-                                         members=members,
-                                         songs=songs,
-                                         matrix_list=matrix_list,
-                                         genres=X_genres)
+    X_val, y_val = utils.merge_it_all_together(df_val,
+                                               diamond=diamond,
+                                               members=members,
+                                               songs=songs,
+                                               matrix_list=matrix_list,
+                                               genres=X_genres)
     del df_val
     D_val = xgb.DMatrix(sparse.hstack([X_val, X_source[utils.TTS:, :]]))
     logging.info('IT WORKED')
     logging.info('merging training data together')
-    X_train, y_train = merge_it_all_together(df_train,
-                                             diamond=diamond,
-                                             members=members,
-                                             songs=songs,
-                                             genres=X_genres)
+    X_train, y_train = utils.merge_it_all_together(df_train,
+                                                   diamond=diamond,
+                                                   members=members,
+                                                   songs=songs,
+                                                   genres=X_genres)
     del df_train
     D_train = xgb.DMatrix(sparse.hstack([X_train, X_source[:utils.TTS, :]]),
                           y_train)
     del X_train, y_train
     # # TODO Use 5-fold CV and grid search to estimate hyperparameters
     logging.info('fitting xgboost model using diamond predictions')
-    model = xgb.train(xgb_params(), D_train, MAX_ITS)
+    model = xgb.train(utils.xgb_params(), D_train, MAX_ITS)
     logging.info("AUC = %f", roc_auc_score(y_val, model.predict(D_val)))
 
-
-def train_test_split(df_train):
-    TTS = 50000
-    df_val = df_train.iloc[TTS:, :]
-    df_train = df_train.iloc[:TTS, :]
-    return df_train, df_val
-
-
-def fit_diamond_model(df_train):
-    path = 'models/diamond_partial.p'
-    if os.path.exists(path):
-        logging.info('reading saved diamond model')
-        with open(path, 'rb') as ff:
-            diamond_model = pickle.load(ff)
-    else:
-        logging.info('fitting diamond model')
-        formula = 'target ~ 1 + (1|song_id) + (1|msno)'
-        priors = pd.DataFrame({'group': ['song_id', 'msno'],
-                               'var1': ['intercept'] * 2,
-                               'var2': [np.nan] * 2,
-                               # fit on a sample of data in R/lme4
-                               'vcov': [0.00845, 0.07268]})
-        diamond_model = LogisticRegression(df_train, priors)
-        diamond_model.fit(formula, tol=1e-5, verbose=False, max_its=200)
-        with open(path, 'wb') as ff:
-            pickle.dump(diamond_model, ff)
-    df_train.drop(['row_index', 'intercept'], axis=1, inplace=True, errors='ignore')
-    return diamond_model
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,177 @@
+import numpy as np
+import pandas as pd
+
+from scipy import sparse
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.feature_extraction import DictVectorizer
+from diamond.glms.logistic import LogisticRegression
+
+import dill
+import os
+import logging
+
+
+
+class ExtraInfo(object):
+    """ holds info about songs or members for making recs """
+    def __init__(self, df):
+        self.df = df
+        self.encoding = None
+        self.id_map = None
+
+    def create_encoding(self, cat_features, id_col):
+        """
+        one hot encode categorical features. modifies `self.df` in-place
+        """
+        idx = id_col + 'x'
+        for x in cat_features:
+            self.df[x] = self.df[x].astype(str).fillna('unknown')
+        self.id_map = id_to_index(self.df[id_col])
+        self.df[idx] = self.df[id_col].apply(lambda x: self.id_map[x])
+        cats = self.df[cat_features + [idx]].sort_values(idx)
+        self.X = DictVectorizer().fit_transform(cats.T.to_dict().values())
+
+    def save(self, f):
+        with open(f, 'wb') as ff:
+            dill.dump(self, ff)
+
+    def load(self, f):
+        with open(f, 'rb') as ff:
+            self.__dict__.update(dill.load(ff).__dict__)
+
+
+def id_to_index(ids):
+    " return a dict like {id: idx} "
+    unique_ids = set(ids)
+    return dict(zip(unique_ids, np.arange(0, len(unique_ids))))
+
+
+def encode_genres(song_map, df_songs):
+    " this is a many-hot encoding of song to genre "
+    path = 'data/processed/genres.dill'
+    if os.path.exists(path):
+        logging.info('reading genre encodings from disk')
+        with open(path, 'rb') as ff:
+            X_genres = dill.load(ff)
+    else:
+        logging.info('creating genre encodings')
+        df_genres = df_songs['genre_ids'].apply(lambda s: pd.Series(str(s).split('|')))
+        df_genres['song_idx'] = df_songs['song_id'].apply(lambda x: song_map[x])
+        df_genres2 = pd.melt(df_genres, 'song_idx', value_name='genre').\
+            drop('variable', axis=1).\
+            dropna().\
+            sort_values('song_idx')
+        genre_map = id_to_index(df_genres2['genre'])
+        df_genres2['genre_idx'] = df_genres2['genre'].apply(lambda g: genre_map[g])
+        X_genres = sparse.coo_matrix((np.ones(len(df_genres2)),
+                                     (df_genres2['song_idx'], df_genres2['genre_idx'])))
+        X_genres = X_genres.tocsr()
+        with open(path, 'wb') as ff:
+            dill.dump(X_genres, ff)
+    return X_genres
+
+
+def xgb_params():
+    xgb_params = {}
+    xgb_params['objective'] = 'binary:logistic'
+    xgb_params['eta'] = 0.75
+    xgb_params['max_depth'] = 5
+    xgb_params['silent'] = 1
+    xgb_params['eval_metric'] = 'auc'
+    return xgb_params
+
+def merge_it_all_together(df,
+                          songs,
+                          genres,
+                          members,
+                          matrix_list,
+                          diamond):
+    """
+    Merge member, song, and source data together
+    Return (X, y) tuple
+    This makes a _copy_ of df
+    """
+
+    # there are 80 interactions with unknown songs in the training set
+    # remove them!
+    logging.info('merging in song info')
+    df = pd.merge(df.drop(SOURCE_FEATURES, axis=1),
+                  songs.df[['song_id', 'song_idx', 'song_length']],
+                  'inner',
+                  'song_id')
+    logging.info('merging in member info')
+    member_features = ['msno', 'msnox', 'registration_init_time',
+                       'expiration_date', 'bd']
+    df = pd.merge(df, members.df[member_features], how='left', on='msno')
+    n_nulls = df.isnull().sum()
+    assert n_nulls.sum() == 0, n_nulls
+    logging.info('there are no nulls')
+    numeric_features = ['song_length', 'registration_init_time',
+                        'expiration_date', 'bd']
+    X_numeric = df[numeric_features].as_matrix()
+    member_idx = df['msnox']
+    song_idx = df['song_idx']
+    y = df['target']
+    del df  # free up the memory
+    logging.info('adding numeric feature matrix')
+    matrix_list.append(X_numeric)
+    logging.info('indexing song matrix')
+    matrix_list.append(songs.X[song_idx, :])
+    logging.info('indexing genre matrix')
+    matrix_list.append(genres[song_idx, :])
+    logging.info('indexing member matrix')
+    matrix_list.append(members.X[member_idx, :])
+    logging.info('concatenating matrices')
+    X = sparse.hstack(matrix_list)
+    return X, y
+
+
+def get_source_encoding(source_features, df):
+    path = 'data/processed/source_features.dill'
+    if os.path.exists(path):
+        logging.info('reading source feature encodings from disk')
+        with open(path, 'rb') as ff:
+            X_source = dill.load(ff)
+    else:
+        logging.info('creating source features encoding')
+        LE, OHE = LabelEncoder(), OneHotEncoder()
+        X_source = OHE.fit_transform(df[source_features].\
+            fillna('unknown').\
+            apply(LE.fit_transform))
+        with open(path, 'wb') as ff:
+            dill.dump(X_source, ff)
+    return X_source
+
+
+def train_test_split(df_train):
+    """
+    split labeled data into 2/3 train, 1/3 validation
+    this reflects that the unlabeled test data is about
+    half the size of the labeled data
+    """
+    TTS = 4918279
+    df_val = df_train.iloc[TTS:, :]
+    df_train = df_train.iloc[:TTS, :]
+    return df_train, df_val
+
+
+def fit_diamond_model(df_train):
+    path = 'models/diamond_partial.p'
+    if os.path.exists(path):
+        logging.info('reading saved diamond model')
+        with open(path, 'rb') as ff:
+            diamond_model = pickle.load(ff)
+    else:
+        logging.info('fitting diamond model')
+        formula = 'target ~ 1 + (1|song_id) + (1|msno)'
+        priors = pd.DataFrame({'group': ['song_id', 'msno'],
+                               'var1': ['intercept'] * 2,
+                               'var2': [np.nan] * 2,
+                               # fit on a sample of data in R/lme4
+                               'vcov': [0.00845, 0.07268]})
+        diamond_model = LogisticRegression(df_train, priors)
+        diamond_model.fit(formula, tol=1e-5, verbose=False, max_its=200)
+        with open(path, 'wb') as ff:
+            pickle.dump(diamond_model, ff)
+    df_train.drop(['row_index', 'intercept'], axis=1, inplace=True, errors='ignore')
+    return diamond_model
